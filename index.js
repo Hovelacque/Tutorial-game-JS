f74d00ac77ff5eac54fc71345660985b17ae6415
Gustavo <gustavo@kapibara.com.br>
2023-07-13 08:40:01 -0300

    Carregando mapa no canvas

---
+++ index.js
@@ -0,0 +1,14 @@
+const canvas = document.querySelector('canvas');
+const context2D = canvas.getContext('2d');
+
+canvas.width = 1024;
+canvas.height = 576;
+
+context2D.fillStyle = 'white';
+context2D.fillRect(0, 0, canvas.width, canvas.height);
+
+const mapaImage = new Image();
+mapaImage.src = "./Map/Mapa-zoom-550.png";
+mapaImage.onload = () => {
+    context2D.drawImage(mapaImage, -1800, -1000);
+}
