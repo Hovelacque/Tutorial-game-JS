6131b83098a07dc773a6c332fcf56c5d382fcf1f
Hovelacque <gustavoinfomax@yahoo.com.br>
2023-12-10 11:53:35 -0300

    Adicionando personagem

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ context2D.fillRect(0, 0, canvas.width, canvas.height);
 const mapaImage = new Image();
 mapaImage.src = "./Map/Mapa-zoom-550.png";
 
+const playerImage = new Image();
+playerImage.src = "./Tilesets/ACharDown.png";
+// playerImage.src = "./Tilesets/playerDwon.png";
+
 let backgroundX = -1800;
 let backgroundY = -1000;
 let velocidade = 5;
@@ -17,6 +21,34 @@ let velocidade = 5;
 function animate() {
     window.requestAnimationFrame(animate);
     context2D.drawImage(mapaImage, backgroundX, backgroundY);
+ 
+    // context2D.drawImage(
+    //     playerImage,
+    //     //cropping
+    //     0, //x crop
+    //     0, //y crop
+    //     // playerImage.width / 2,
+    //     // playerImage.height,
+    //     // //x e y
+    //     canvas.width / 2 - playerImage.width / 2,
+    //     canvas.height / 2 - playerImage.height / 2,
+    //     // playerImage.width / 2,
+    //     // playerImage.height
+    // );
+
+    // context2D.drawImage(
+    //     playerImage,
+    //     //cropping
+    //     0, //x crop
+    //     0, //y crop
+    //     playerImage.width / 2,
+    //     playerImage.height / 2,
+    //     //x e y
+    //     canvas.width / 2 - playerImage.width / 2,
+    //     canvas.height / 2 - playerImage.height / 2,
+    //     playerImage.width / 2,
+    //     playerImage.height / 2
+    // );
 }
 
 window.addEventListener('keydown', (e) => {
