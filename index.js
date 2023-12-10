be8eda1ba2c5c6c23a34de6b926106ceac64a64f
Hovelacque <gustavoinfomax@yahoo.com.br>
2023-12-10 13:57:09 -0300

    Cortando personagem

--- index.js
+++ index.js
@@ -14,41 +14,44 @@ const playerImage = new Image();
 playerImage.src = "./Tilesets/ACharDown.png";
 // playerImage.src = "./Tilesets/playerDwon.png";
 
-let backgroundX = -1800;
+let backgroundX = -1575;
 let backgroundY = -1000;
-let velocidade = 5;
+let velocidade = 10;
 
 function animate() {
     window.requestAnimationFrame(animate);
     context2D.drawImage(mapaImage, backgroundX, backgroundY);
- 
+
+    /* PLOT SIMPLES */
     // context2D.drawImage(
     //     playerImage,
-    //     //cropping
-    //     0, //x crop
-    //     0, //y crop
-    //     // playerImage.width / 2,
-    //     // playerImage.height,
-    //     // //x e y
-    //     canvas.width / 2 - playerImage.width / 2,
-    //     canvas.height / 2 - playerImage.height / 2,
-    //     // playerImage.width / 2,
-    //     // playerImage.height
+    //     0, //x crop (dx)
+    //     0, //y crop (dy)
+    //     canvas.width / 2 - playerImage.width / 2, // (dw)
+    //     canvas.height / 2 - playerImage.height / 2, // (dh)
     // );
 
+    /* PLOT COM CUT */
     // context2D.drawImage(
     //     playerImage,
     //     //cropping
-    //     0, //x crop
-    //     0, //y crop
-    //     playerImage.width / 2,
-    //     playerImage.height / 2,
-    //     //x e y
-    //     canvas.width / 2 - playerImage.width / 2,
-    //     canvas.height / 2 - playerImage.height / 2,
-    //     playerImage.width / 2,
-    //     playerImage.height / 2
+    //     0, // sx (x superior esquerdo)
+    //     0, // sy (y superior esquerdo)
+    //     playerImage.width / 2, // sw (largura corte) 
+    //     playerImage.height / 2, // sh (altura corte)
+    //     0,  // dx (x para plot)
+    //     0,  // dy (y para plot)
+    //     100, // dw (largura para plot)
+    //     100  // dh (altura para plot)
     // );
+
+    context2D.drawImage(
+        playerImage,
+        0, 0, playerImage.width / 2, playerImage.height / 2, //cuting
+        canvas.width / 2 - playerImage.width / 2, //x
+        canvas.height / 2 - playerImage.height /2, //y
+        100, 100 //tamanho player
+    );
 }
 
 window.addEventListener('keydown', (e) => {
