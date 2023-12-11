0496cd87fabe2c3ca33b4656a6618a2bd6bca918
Hovelacque <gustavoinfomax@yahoo.com.br>
2023-12-11 13:32:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -18,9 +18,47 @@ let backgroundX = -1575;
 let backgroundY = -1000;
 let velocidade = 10;
 
+class Sprite {
+    constructor(image, x, y, velocidade) {
+        this.image = image;
+        this.x = x;
+        this.y = y;
+        this.velocidade = velocidade;
+    }
+
+    draw() {
+        // context2D.drawImage(this.image, backgroundX, backgroundY);
+        context2D.drawImage(this.image, this.x, this.y);
+    }
+}
+
+const background = new Sprite(mapaImage, backgroundX, backgroundY, velocidade);
+
+const movimentos = {
+    cima: false,
+    baixo: false,
+    esquerda: false,
+    direita: false
+    // cima: {
+    //     press: false
+    // },
+    // baixo: {
+    //     press: false
+    // },
+    // esquerda: {
+    //     press: false
+    // },
+    // direita: {
+    //     press: false
+    // }
+}
+
+let frame = 1;
+
 function animate() {
     window.requestAnimationFrame(animate);
-    context2D.drawImage(mapaImage, backgroundX, backgroundY);
+
+    background.draw();
 
     /* PLOT SIMPLES */
     // context2D.drawImage(
@@ -45,33 +83,114 @@ function animate() {
     //     100  // dh (altura para plot)
     // );
 
+    let sx = 0, sy = 0, sw = playerImage.width / 2, sh = playerImage.height / 2;
+    // if (frame == 2) {
+        // sx = playerImage.width / 2;
+        // sy = 0;
+        // sw = playerImage.width;
+        // sh = playerImage.height / 2;
+        // frame++;
+    // }
+    // else if (frame == 3) {
+        // sx = 0;
+        // sy = playerImage.height / 2;
+        // sw = playerImage.width / 2;
+        // sh = playerImage.height;
+    //     frame++;
+    // }
+    // else if (frame == 4) {
+        sx = playerImage.width / 2;
+        sy = playerImage.height / 2;
+        sw = playerImage.width;
+        sh = playerImage.height;
+    //     frame = 1;
+    // }
+    // else
+    //     frame++
+
     context2D.drawImage(
         playerImage,
-        0, 0, playerImage.width / 2, playerImage.height / 2, //cuting
+        // 0, 0, playerImage.width / 2, playerImage.height / 2, //cuting
+        sx, sy, sw, sh,
         canvas.width / 2 - playerImage.width / 2, //x
-        canvas.height / 2 - playerImage.height /2, //y
+        canvas.height / 2 - playerImage.height / 2, //y
         100, 100 //tamanho player
     );
+
+    if (movimentos.cima) {
+        background.y += velocidade;
+    }
+    if (movimentos.esquerda) {
+        background.x += velocidade;
+    }
+    if (movimentos.baixo) {
+        background.y -= velocidade;
+    }
+    if (movimentos.direita) {
+        background.x -= velocidade;
+    }
+    // if (movimentos.cima.press) {
+    //     background.y += velocidade;
+    // }
+    // if (movimentos.esquerda.press) {
+    //     background.x += velocidade;
+    // }
+    // if (movimentos.baixo.press) {
+    //     background.y -= velocidade;
+    // }
+    // if (movimentos.direita.press) {
+    //     background.x -= velocidade;
+    // }
+
+    // background.draw();
+
 }
 
 window.addEventListener('keydown', (e) => {
     // console.log(e.key)
+    console.log(movimentos);
     switch (e.key) {
         case 'w':
         case 'ArrowUp':
             backgroundY += velocidade;
+            movimentos.cima = true;
             break;
         case 'a':
         case 'ArrowLeft':
             backgroundX += velocidade;
+            movimentos.esquerda = true;
             break;
         case 's':
         case 'ArrowDown':
             backgroundY -= velocidade;
+            movimentos.baixo = true;
             break;
         case 'd':
         case 'ArrowRight':
             backgroundX -= velocidade;
+            movimentos.direita = true;
+            break;
+    }
+})
+
+window.addEventListener('keyup', (e) => {
+    console.log(movimentos);
+    switch (e.key) {
+        case 'w':
+        case 'ArrowUp':
+            movimentos.cima = false;
+            break;
+        case 'a':
+        case 'ArrowLeft':
+            movimentos.esquerda = false;
+            break;
+        case 's':
+        case 'ArrowDown':
+            movimentos.baixo = false;
+            break;
+        case 'd':
+        case 'ArrowRight':
+            movimentos.direita = false;
             break;
     }
 })
